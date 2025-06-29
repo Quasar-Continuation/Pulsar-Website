7c000d61295362470687930188d86cd21ad2653f
KingKDot <why_are_you_looking_at_this@stoppeeping.com>
2025-06-28 19:54:41 -0500

    add simple analytics because gitlab doesn't supply any.

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <script data-collect-dnt="true" async src="https://scripts.simpleanalyticscdn.com/latest.js"></script>
+      </head>
       <body className={inter.className}>
         <ThemeProvider attribute="class" defaultTheme="dark" enableSystem disableTransitionOnChange>
           {children}
